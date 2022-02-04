83a1867d36313268d1461bac8100511d4a48477f
Justin Hoang <justinhoang90@gmail.com>
2022-02-04 13:20:19 -0800

    Adding data of solar system

--- src/planet-data/planets.js
+++ src/planet-data/planets.js
@@ -1,29 +1,90 @@
-const planets = [
+const solarSystemInfo = [
   {
     id: 1,
+    name: `Sun`,
+    radius: `696,340 km`,
+    surface_area: `6.08 trillion km²`,
+    orbital_time: null,
+    desc: `The Sun is the star at the center of the Solar System. It is a nearly perfect ball of hot plasma, 
+      heated to incandescence by nuclear fusion reactions in its core, radiating the energy mainly as visible light, 
+      ultraviolet light, and infrared radiation. It is by far the most important source of energy for life on Earth.`,
   },
   {
     id: 2,
+    name: `Mercury`,
+    radius: `2,439.7km`,
+    surface_area: `74.8 million km²`,
+    orbital_time: `88 days`,
+    desc: `The smallest planet in our solar system and closest to the Sun—is only slightly larger than Earth's Moon. 
+      Mercury is the fastest planet, zipping around the Sun every 88 Earth days.`
   },
   {
     id: 3,
+    name: `Venus`,
+    radius: `6,051.8 km`,
+    surface_area: `460.2 million km²`,
+    orbital_time: `225 days`,
+    desc: `Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. 
+    As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be 
+    visible to the naked eye in broad daylight.`
   },
   {
     id: 4,
+    name: `Earth`,
+    radius: `6,371 km`,
+    surface_area: `510.1 million km²`,
+    orbital_time: `365 days`,
+    desc: `Earth is the third planet from the Sun and the only astronomical object known to harbor life. 
+      While large amounts of water can be found throughout the Solar System, only Earth sustains liquid surface water. 
+      About 71% of Earth's surface is made up of the ocean, dwarfing Earth's polar ice, lakes and rivers.`
   },
   {
     id: 5,
+    name: `Mars`,
+    radius: `3,389.5 km`,
+    surface_area: `144.8 million km²`,
+    orbital_time: `687 days`,
+    decs: `Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only 
+      Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet"`
   },
   {
     id: 6,
+    name: `Jupiter`,
+    radius: `69,911 km`,
+    surface_area: `61.42 billion km²`,
+    orbital_time: `12 years`,
+    decs: `Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass 
+      more than two and a half times that of all the other planets in the Solar System combined, but slightly less than 
+      one-thousandth the mass of the Sun.`
   },
   {
     id: 7,
+    name: `Saturn`,
+    radius: `58,232 km`,
+    surface_area: `42.7 billion km²`,
+    orbital_time: `29 years`,
+    decs: `Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. 
+      It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth 
+      the average density of Earth; however, with its larger volume, Saturn is over 95 times more massive.`
   },
   {
     id: 8,
+    name: `Uranus`,
+    radius: `25,362 km`,
+    surface_area: `8.083 billion km²`,
+    orbital_time: `84 years`,
+    decs: `Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus, who, 
+      according to Greek mythology, was the great-grandfather of Ares, grandfather of Zeus and father of Cronus. 
+      It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.`
   },
   {
     id: 9,
+    name: `Neptune`,
+    radius: `24,622 km`,
+    surface_area: `7.618 billion km²`,
+    orbital_time: `165 years`,
+    decs: `Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest 
+      planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, 
+      slightly more massive than its near-twin Uranus.`
   },
 ];
